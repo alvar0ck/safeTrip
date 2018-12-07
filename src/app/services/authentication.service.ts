d45928d1317960dbf23844490b81929a33ffbd5a
Delgado Escobar <a.delgado.escobar@accenture.com>
2018-12-07 16:43:00 -0300

    First commit

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,90 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+import { Platform } from '@ionic/angular';
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { BehaviorSubject } from 'rxjs';
+ 
+const TOKEN_KEY = 'auth-token';
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+ 
+  authenticationState = new BehaviorSubject(false);
+ 
+  constructor(private storage: Storage, private plt: Platform) { 
+    this.plt.ready().then(() => {
+      this.checkToken();
+    });
+  }
+ 
+  checkToken() {
+    this.storage.get(TOKEN_KEY).then(res => {
+      if (res) {
+        this.authenticationState.next(true);
+      }
+    })
+  }
+ 
+  login() {
+    return this.storage.set(TOKEN_KEY, 'Bearer 1234567').then(() => {
+      this.authenticationState.next(true);
+    });
+  }
+ 
+  logout() {
+    return this.storage.remove(TOKEN_KEY).then(() => {
+      this.authenticationState.next(false);
+    });
+  }
+ 
+  isAuthenticated() {
+    return this.authenticationState.value;
+  }
+ 
+}
